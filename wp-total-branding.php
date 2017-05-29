efb1c8025eecb505963845aed3588318fd375804
mustafauysal <m_uysl@hotmail.com>
2017-05-30 01:38:13 +0300

    initial commit

---
+++ wp-total-branding.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Wp Total Branding
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-total-branding
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Total_Branding
+ */
