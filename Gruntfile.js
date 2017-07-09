077af60f777eab308e436377ad31dd0b71b1a9e1
mustafauysal <m_uysl@hotmail.com>
2017-07-09 18:25:01 +0300

    Gruntfile update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,19 @@
-module.exports = function( grunt ) {
+module.exports = function (grunt) {
 
 	'use strict';
 	var banner = '/**\n * <%= pkg.homepage %>\n * Copyright (c) <%= grunt.template.today("yyyy") %>\n * This file is generated automatically. Do not edit.\n */\n';
 	// Project configuration
-	grunt.initConfig( {
+	grunt.initConfig({
 
-		pkg: grunt.file.readJSON( 'package.json' ),
+		pkg: grunt.file.readJSON('package.json'),
 
 		addtextdomain: {
 			options: {
 				textdomain: 'wp-total-branding',
 			},
-			target: {
+			target : {
 				files: {
-					src: [ '*.php', '**/*.php', '!node_modules/**', '!php-tests/**', '!bin/**' ]
+					src: ['*.php', '**/*.php', '!node_modules/**', '!php-tests/**', '!includes/libs/**', '!bin/**']
 				}
 			}
 		},
@@ -29,24 +29,25 @@ module.exports = function( grunt ) {
 		makepot: {
 			target: {
 				options: {
-					domainPath: '/languages',
-					mainFile: 'wp-total-branding.php',
-					potFilename: 'wp-total-branding.pot',
-					potHeaders: {
-						poedit: true,
+					domainPath     : '/languages',
+					mainFile       : 'wp-total-branding.php',
+					potFilename    : 'wp-total-branding.pot',
+					exclude        : ['includes/libs/*'],
+					potHeaders     : {
+						poedit                 : true,
 						'x-poedit-keywordslist': true
 					},
-					type: 'wp-plugin',
+					type           : 'wp-plugin',
 					updateTimestamp: true
 				}
 			}
 		},
-	} );
+	});
 
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
-	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
-	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
-	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
+	grunt.loadNpmTasks('grunt-wp-i18n');
+	grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
+	grunt.registerTask('i18n', ['addtextdomain', 'makepot']);
+	grunt.registerTask('readme', ['wp_readme_to_markdown']);
 
 	grunt.util.linefeed = '\n';
 
