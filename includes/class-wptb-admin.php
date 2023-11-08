da49688b117fcab271266b95032d485910bf295a
mustafauysal <me@uysalmustafa.com>
2023-11-08 13:30:42 +0300

    fix deprecated calls & hide redux related stuff

--- includes/class-wptb-admin.php
+++ includes/class-wptb-admin.php
@@ -9,20 +9,20 @@ class WPTB_Admin {
 	function __construct() { }
 
 	function setup() {
-
 		if ( ! class_exists( 'ReduxFramework' ) && file_exists( WPTB_INC_DIR . 'libs/redux-core/framework.php' ) ) {
 			require_once( WPTB_INC_DIR . 'libs/redux-core/framework.php' );
 		}
 		$redux = ReduxFrameworkInstances::get_instance( WPTB_OPTION );
 
 
-		Redux::setExtensions( WPTB_OPTION, WPTB_INC_DIR . 'libs/redux-vendor-support/vendor_support/extension_vendor_support.php' );
+		Redux::set_extensions( WPTB_OPTION, WPTB_INC_DIR . 'libs/redux-vendor-support/vendor_support/extension_vendor_support.php' );
 
 		if ( ! isset( $redux_demo ) && file_exists( WPTB_INC_DIR . 'settings.php' ) ) {
 			require_once( WPTB_INC_DIR . 'settings.php' );
 		}
+		Redux::disable_demo();
 
-		add_action( 'admin_menu', array( $this, 'set_global_menu' ) );
+		add_action( 'admin_menu', array( $this, 'set_global_menu' ), 999 );
 
 		add_action( 'redux/loaded', array( $this, 'maybe_remove_admin_menu' ) );
 	}
@@ -40,6 +40,7 @@ class WPTB_Admin {
 			update_site_option( 'wptb_admin_menu', $menu );
 		}
 
+		remove_submenu_page('options-general.php', 'redux-framework');
 	}
 
 	/**
